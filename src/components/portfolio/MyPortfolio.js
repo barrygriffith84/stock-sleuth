4c1531430b139313b70d6b7f4efe8e791f69a7fa
Barry Griffith <barrygriffith@outlook.com>
2020-06-19 15:33:29 -0400

    Table prints to the MyPortfolio page, name of user is in the h1 element

--- src/components/portfolio/MyPortfolio.js
+++ src/components/portfolio/MyPortfolio.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
+import APIManager from '../../modules/APIManager'
+import PortfolioTable from './PortfolioTable'
+
 
 class MyPortfolio extends Component {
     
+    state = {
+        stockPurchases: [],
+        username: "",
+    }
+
+
+    componentDidMount() {
+        APIManager.getPortfolio("1").then((APIPurchases) => 
+        { console.log(APIPurchases); this.setState({
+            stockPurchases: APIPurchases,
+            username: APIPurchases[0].user.username
+        })})
+       
+    }
+
 render () {
 
     return(
         <>
-        <h1>This is My Portfolio!</h1>
+        <h1>This is {this.state.username.charAt(0).toUpperCase() + this.state.username.slice(1)}'s portfolio!</h1>
+        <div className="test-container">
+    <PortfolioTable purchases={this.state.stockPurchases}/>
+        </div>
         </>
     )
 }
---
+++ src/modules/APIManager.js
@@ -0,0 +1,19 @@
+import APIKeys from './APIKeys'
+
+const APIManager = {
+    
+    symbolSearch(companyName) {
+        return fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${companyName}&apikey=${APIKeys.alphaKey}`).then((r) => {r.json()})
+    },
+
+    getPortfolio(userId) {
+        return fetch(`http://localhost:5002/purchases?userId=${userId}&_expand=user`).then((r) => r.json())
+    },
+    
+    getPortfolioPrices(stockArray) {
+        return fetch(`https://marketdata.websol.barchart.com/getQuote.json?${APIKeys.barchartKey}=&symbols=${stockArray}&fields=fiftyTwoWkHigh%2CfiftyTwoWkHighDate%2CfiftyTwoWkLow%2CfiftyTwoWkLowDate`).then((r) => r.json())
+    }
+}
+
+
+export default APIManager
